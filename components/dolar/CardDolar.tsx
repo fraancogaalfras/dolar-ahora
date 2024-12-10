3a0941927b857715710808a74a3a7174d4f320b2
ruloosgaalfras <franco.galfras1@gmail.com>
2024-12-10 18:15:50 -0300

    added black icon logo and black background in splash screen

--- components/dolar/CardDolar.tsx
+++ components/dolar/CardDolar.tsx
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 10 },
     shadowOpacity: 1,
     shadowRadius: 10,
-    elevation: 10,
+    elevation: 5,
   },
   text: {
     color: 'white',
